5a9abc3075101df09003b8d3aa3acefc0ecb7e2a
Edward Chen <echen333us@gmail.com>
2022-07-03 23:42:52 -0400

    taskbar eh formatting, doing fs now

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
+import { AiOutlineSearch } from 'react-icons/ai'
+import { BsThreeDotsVertical } from 'react-icons/bs'
 
 function App() {
 
   const [files, setFiles] = useState([
     {names:"root", depth:"ml-0", par_id:0, id:1, child_id:[2]}, 
     {names:"test", depth:"ml-5", par_id:1, id:2, child_id:[]}]);
+  const [curId, setCurID] = useState(1);
   // const [files, setFiles] = useState([{names:"root", depth:0}, "test"]);
 
   return (
     <div>
-      <div className="flex h-10 mt-1 mb-3 border-blue-400 border-solid border-w border-b-4">
-        <div className="bg-pink-300">
+      <div className="flex h-10 mt-1 mb-3 items-center">
+        <img src={require('./img/chromeIcon.png')} className="h-7 w-7 mx-4"></img>
+        <div className="font-semibold text-lg font-mono">
           Bookmarks
         </div>
-        <input className="border-gray-300 rounded-3xl flex-1 ml-8 bg-fuchsia-400" type="search"/>
-        <button>Press</button>
+        <div className="border-gray-300 rounded-3xl flex flex-1 ml-8 z-10 h-full bg-gray-100 
+             text-sm mx-3 ">
+          <AiOutlineSearch className="h-6 w-6 hover:bg-gray-300 rounded-full items-center"/>
+          <input className="pl-3 focus:outline-none bg-gray-100"
+            placeholder="Search bookmarks"
+            type="search"/>
+        </div>
+        <BsThreeDotsVertical className="mx-3 hover:cursor-pointer hover:bg-gray-300 rounded-full h-6 w-6" size={20}/>
       </div>
 
-      <div className="flex flex-col w-20 bg-slate-400 h-screen">
-        Bookmarks
+      <div className="fixed w-32 h-screen">
         {
           files.map( (x,ind) => {
             return <File key={ind} val={x}/>
@@ -28,9 +37,8 @@ function App() {
         }
       </div>
 
-      <div className="bg-red-300 inline-block">
-        Hello
-      </div>
+      <Viewport content={files.find( x => x.id === curId)}/>
+      
     </div>
   );
 }
@@ -42,10 +50,29 @@ function File({val}) {
   }
 
   return <div className={val.depth}>
-    <div className="bg-orange-200">
+    <div className="bg-orange-200 hover:bg-blue-300 rounded-r-xl h-10">
       {val.names}
     </div>
   </div>
 }
 
+function Viewport({content}){
+  console.log(content);
+  //files search for id
+  return (
+    <div className="bg-red-300 inline-block ml-36 mr-10">
+      <div className="border-blue-300 w-screen h-40 border-4 rounded-lg">
+        {content.child_id.map( x => {
+          return <Card cardID={x}/>
+        })}
+      </div>
+    </div>
+  )
+}
+
+function Card({cardID}){
+  return <div>
+    HI
+  </div>
+}
 export default App;
